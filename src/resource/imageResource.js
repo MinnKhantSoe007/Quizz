fc8f1510ae73f2cc5702bb7bdeda01e4299823d4
MinnKhantSoe007 <minnkhantsoe2@gmail.com>
2023-08-01 00:22:25 +0630

    Optimizing and Building apk

--- src/resource/imageResource.js
+++ src/resource/imageResource.js
@@ -1,9 +1,9 @@
 export const ImageResource = {
 
   logo: {
-    home_logo: require('../../assets/photos/img_main.png'),
-    splash_logo: require('../../assets/photos/img_splash.png'),
-    congraz_logo: require('../../assets/photos/img_congraz.png'),
-    loose_logo: require('../../assets/photos/img_loose.png'),
+    home_logo: require('../../assets/photos/main.png'),
+    splash_logo: require('../../assets/photos/splash.png'),
+    congraz_logo: require('../../assets/photos/congraz.png'),
+    loose_logo: require('../../assets/photos/loose.png'),
   }
 }
